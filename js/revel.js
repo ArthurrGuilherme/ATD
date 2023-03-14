3c15a77142e285d397ddd0715f591deeca60f8a6
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2023-03-13 23:35:46 -0300

    Add files via upload

--- js/revel.js
+++ js/revel.js
@@ -7,9 +7,9 @@ sr.reveal('#organicao-primaria', { duration: 2500});
 /*Index*/
 window.sr = ScrollReveal({ reset: true});
 
-sr.reveal('.organicao-primaria', { duration: 2000});
+sr.reveal('.organicao-primaria', { duration: 2500});
 sr.reveal('.container-terceiro', { duration: 2500});
-sr.reveal('.sessao-empresas', { duration: 2500,
-    rotate: {x:80, y:0, z:0}
+sr.reveal('.sessao-empresas', { duration: 2900,
+    rotate: {x:0, y:80, z:0}
 });
-sr.reveal('.container_metodologia', { duration: 2500});
+sr.reveal('.container_metodologia', { duration: 2900});
